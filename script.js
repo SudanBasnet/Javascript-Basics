94ce46852280c8283703c4d999cdd032cd9d8dbf
Sudan Basnet <mysticjuku@Sudans-MacBook-Pro-2.local>
2026-04-04 23:46:39 +1100

    variables

---
+++ script.js
@@ -0,0 +1,22 @@
+// console.log("sudan");
+// alert("hi there, please click ok to complete the rendering");
+
+//=====variable (only char, num, $)
+
+//var , let , const
+
+var name; //creating variale
+name = " sudan Basnet"; //assigning value
+
+let add = "sydney";
+console.log(name, add);
+
+name = "asmita";
+console.log(name);
+add = "melbourne";
+console.log(add);
+
+// age= 20; //if we don't declare anything like var or let or const.. it will automatically consider as var
+const age = 20;
+age = 5;
+console.log(age); //can't be changed
