2423175862cbc20c48156f8bb1c9e140d9cbfdb6
Sudan Basnet <mysticjuku@Sudans-MacBook-Pro-2.local>
2026-04-04 23:59:38 +1100

    arithmetic operations

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-// console.log("sudan");
-// alert("hi there, please click ok to complete the rendering");
+// // console.log("sudan");
+// // alert("hi there, please click ok to complete the rendering");
 
-//=====variable (only char, num, $)
+// //=====variable (only char, num, $ _)
 
-//var , let , const
+// //var , let , const
 
-var name; //creating variale
-name = " sudan Basnet"; //assigning value
+// var name; //creating variale
+// name = " sudan Basnet"; //assigning value
 
-let add = "sydney";
-console.log(name, add);
+// let add = "sydney";
+// console.log(name, add);
 
-name = "asmita";
-console.log(name);
-add = "melbourne";
-console.log(add);
+// name = "asmita";
+// console.log(name);
+// add = "melbourne";
+// console.log(add);
 
-// age= 20; //if we don't declare anything like var or let or const.. it will automatically consider as var
-const age = 20;
-age = 5;
-console.log(age); //can't be changed
+// // age= 20; //if we don't declare anything like var or let or const.. it will automatically consider as var
+// const age = 20;
+// age = 5;
+// console.log(age); //can't be changed
+
+//======arithmetic
+
+const a = 5;
+const b = 6;
+const c = b;
+
+// +, -, *, **, /,%,++,--
+const result = a + b + c;
+
+console.log(result);
