fef9db4fb8df28f8be255780aed9da2eca3e9034
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-09 15:29:57 +1000

    clouser

--- advance-script.js
+++ advance-script.js
@@ -19,13 +19,38 @@
 
 //how and where you create a variable determines the hoisting
 
-function f() {
-  let a = "a";
-  if (true) {
-    let a = "zzz";
-    console.log(a);
-  }
-  console.log(a);
-}
+// function f() {
+//   let a = "a";
+//   if (true) {
+//     let a = "zzz";
+//     console.log(a);
+//   }
+//   console.log(a);
+// }
 
-f();
+// f();
+
+//clouser
+//allowing a function to remember and access its lexical scope even when it's executing outside its original scope
+//a function inside a function can have access to the variable defined in the parent function even parent function finish executing
+
+// function f() {
+//   let counter = 0;
+//   return function () {
+//     return counter++;
+//   };
+// }
+
+// const v1 = f();
+// // console.log(v1());
+// // console.log(v1());
+// // console.log(v1());
+// // console.log(v1());
+// // console.log(v1());
+// let counter = 0;
+// for (let i = 0; i < 5; i++) {
+//   function print() {
+//     console.log(i + " but counter is " + counter++);
+//   }
+//   setTimeout(print, 0);
+// }
