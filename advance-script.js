634491e52c0cb01f7472b3d5fea62050c69e8276
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-09 14:26:57 +1000

    hoisting

--- advance-script.js
+++ advance-script.js
@@ -1,14 +1,31 @@
-var globalVar = "global";
+// var globalVar = "global";
 
-function outerFunctionScope() {
-  var outerFunVar = "Outer";
-  function innerFunctionScope() {
-    var innerFunVar = "inner";
-    // console.log(innerFunctionScope);
-    // console.log(outerFunVar);
-    // console.log(globalVar);
+// function outerFunctionScope() {
+//   var outerFunVar = "Outer";
+//   function innerFunctionScope() {
+//     var innerFunVar = "inner";
+//     // console.log(innerFunctionScope);
+//     // console.log(outerFunVar);
+//     // console.log(globalVar);
+//   }
+//   innerFunctionScope();
+// }
+
+// outerFunctionScope();
+
+//Hoisting
+
+//moving variables or function declarartions to the top of their context
+
+//how and where you create a variable determines the hoisting
+
+function f() {
+  let a = "a";
+  if (true) {
+    let a = "zzz";
+    console.log(a);
   }
-  innerFunctionScope();
+  console.log(a);
 }
 
-outerFunctionScope();
+f();
